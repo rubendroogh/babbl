1aa958cbec6f0d6c426ef910be70e0039a7eb805
rubensPC <rubendroogh@hotmail.nl>
2018-09-09 19:18:44 +0200

    No longer using position: fixed for messaging elements

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,28 +2,20 @@
 
 @section('content')
 <div class="container">
-    <div class="row">
-        <div class="col-md-12">
-            <div id="messages">
-                
+    <div id="messages" class="row messages"></div>
+    <form id="messageInput" class="message_input py-3 row">
+        @csrf
+        <input id="user_id" type="hidden" name="_user_id" value="{{ Auth::user()->id }}">
+        <input id="user_name" type="hidden" name="_user_name" value="{{ Auth::user()->name }}">
+            <div class="col-9">
+                <input class="form-control" id="message" type="text" name="message" autofocus>
             </div>
-            <form id="messageInput" class="message_input pt-3">
-                @csrf
-                <input id="user_id" type="hidden" name="_user_id" value="{{ Auth::user()->id }}">
-                <input id="user_name" type="hidden" name="_user_name" value="{{ Auth::user()->name }}">
-                <div class="form-group row">
-                    <div class="col-8">
-                        <input class="form-control" id="message" type="text" name="message" autofocus>
-                    </div>
-                    <div class="col-4">
-                        <button class="btn btn-primary" type="button" id="sendMessageButton" onclick="SendMessage()">
-                            Send
-                        </button>
-                    </div>
-                </div>
-            </form>
-        </div>
-    </div>
+            <div class="col-3">
+                <button class="btn btn-primary" type="button" id="sendMessageButton" onclick="SendMessage()">
+                    Send
+                </button>
+            </div>
+    </form>
 </div>
 <script src="https://js.pusher.com/4.3/pusher.min.js"></script>
 <script src="js/home.js"></script>
