d9bfce53efd8f97496f9bf43b84aaf225b732139
rubensPC <rubendroogh@hotmail.nl>
2018-09-10 21:29:25 +0200

    Fixed issue where messaging input div would be too wide

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,21 +3,17 @@
 @section('content')
 <div class="container">
     <div id="messages" class="row messages"></div>
-    <div class="">
-        <form id="messageInput" class="message_input py-3" onsubmit="return SendMessage();">
-            @csrf
-            <input id="user_id" type="hidden" name="_user_id" value="{{ Auth::user()->id }}">
-            <input id="user_name" type="hidden" name="_user_name" value="{{ Auth::user()->name }}">
-            <div class="form-row">
-                <div class="col-9">
-                    <input class="form-control" id="message" type="text" name="message" autofocus>
-                </div>
-                <div class="col-3">
-                    <input type="submit" class="btn btn-primary" id="sendMessageButton" value="Send">
-                </div>
+    <form id="messageInput" class="message_input py-3 row bg-white" onsubmit="return SendMessage();">
+        @csrf
+        <input id="user_id" type="hidden" name="_user_id" value="{{ Auth::user()->id }}">
+        <input id="user_name" type="hidden" name="_user_name" value="{{ Auth::user()->name }}">
+            <div class="col-9">
+                <input class="form-control" id="message" type="text" name="message" autofocus>
             </div>
-        </form>
-    </div>
+            <div class="col-3">
+                <input type="submit" class="btn btn-primary" id="sendMessageButton" value="Send">
+            </div>
+    </form>
 </div>
 <script src="https://js.pusher.com/4.3/pusher.min.js"></script>
 <script src="js/home.js"></script>
