ddb91a5ba4c53506ec50453e1524243b3dd69ed8
rubendroogh <rubendroogh@hotmail.nl>
2018-09-11 09:50:15 +0200

    Added home view

---
+++ resources/views/home.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-12">
+            <h1>Welkom!</h1>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-12">
+            <a href="{{ Route('messager') }}">Messager</a>
+        </div>
+    </div>
+</div>
+<script src="https://js.pusher.com/4.3/pusher.min.js"></script>
+<script src="js/messager.js"></script>
+@endsection
