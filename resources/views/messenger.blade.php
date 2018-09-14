2072ab9caa2b3fa1141e2421154c97fc9d1e7078
rubensPC <rubendroogh@hotmail.nl>
2018-09-14 18:47:22 +0200

    Show loaded messages as received when actually received

--- resources/views/messenger.blade.php
+++ resources/views/messenger.blade.php
@@ -2,13 +2,15 @@
 
 @section('content')
 <div class="container">
+
     <div id="messages" class="row messages">
-        @foreach( $group->messages as $message )
+        @foreach( $group->messages as $m )
             <div class="fullwidth">
-                <div class="message_sent">{{ $message->content }}</div>
+                <div class="message_{{ $m->user_id == Auth::id() ? 'sent' : 'received' }}">{{ $m->content }}</div>
             </div>
         @endforeach
     </div>
+
     <form id="messageInput" class="message_input py-3 row bg-white" onsubmit="return sendMessage();">
         @csrf
         <input id="user_id" type="hidden" name="_user_id" value="{{ Auth::user()->id }}">
@@ -21,6 +23,7 @@
                 <input type="submit" class="btn btn-primary" id="sendMessageButton" value="Send">
             </div>
     </form>
+
 </div>
 <script src="https://js.pusher.com/4.3/pusher.min.js"></script>
 <script src="/js/messenger.js"></script>
