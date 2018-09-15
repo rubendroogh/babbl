163b9488d5f81452208cbfcfbacbb23a9b51e5f1
rubensPC <rubendroogh@hotmail.nl>
2018-09-15 19:16:41 +0200

    Removed Group function from Home Controller.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,8 +4,6 @@ namespace App\Http\Controllers;
 
 use Auth;
 use App\User;
-use App\Group;
-use App\Message;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -30,33 +28,5 @@ class HomeController extends Controller
         $id = Auth::id();
         $groups = User::find($id)->groups;
         return view('home', ['groups' => $groups]);
-    }
-
-    public function messenger($group_id = 1)
-    {
-        $group = Group::find($group_id);
-        return view('messenger', ['group' => $group]);
-    }
-
-    public function newGroup()
-    {
-        return view('newgroup');
-    }
-
-    public function createNewGroup(Request $request)
-    {
-        $user_ids = explode(',', $request->users);
-        if ( !in_array( Auth::id(), $user_ids, false ) ) {
-            $user_ids[] = Auth::id();
-        }
-
-        $users = User::find($user_ids);     
-        $group = Group::create(['name' => $request->group_name]);
-
-        foreach ($users as $user) {
-            $group->users()->save($user);
-        }
-        
-        return view('messenger', ['group' => $group]);
     }
 }
