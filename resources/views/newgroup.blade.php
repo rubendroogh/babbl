ad3eb517d735fe5faf589a7d4fc1a527c56c10bd
rubensPC <rubendroogh@hotmail.nl>
2018-09-15 18:31:13 +0200

    Added adding users to new group using selectize

--- resources/views/newgroup.blade.php
+++ resources/views/newgroup.blade.php
@@ -7,10 +7,16 @@
         @csrf
         <div class="form-group">
             <label for="groupNameInput">Group name</label>
-            <input name="group_name" type="text" class="form-control" id="groupNameInput" aria-describedby="textHelp" placeholder="Enter name">
-            <small id="textHelp" class="form-text text-muted">Think of something fun and descriptive.</small>
+            <input name="group_name" type="text" class="form-control" id="groupNameInput" aria-describedby="groupNameHelp" placeholder="Enter name">
+            <small id="groupNameHelp" class="form-text text-muted">Think of something fun and descriptive.</small>
+        </div>
+        <div class="form-group">
+            <label for="usersInput">Add users</label>
+            <input name="users" type="text" class="form-control" aria-describedby="userHelp" id="usersInput">
+            <small id="userHelp" class="form-text text-muted">You can search on either name or email.</small>
         </div>
         <button type="submit" class="btn btn-primary">Save</button>
     </form>
 </div>
+<script src="/js/new_group.js"></script>
 @endsection
