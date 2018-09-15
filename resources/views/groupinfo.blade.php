d5f4d961623f8b11801c2db7f8d9b092b2ee88dc
rubensPC <rubendroogh@hotmail.nl>
2018-09-15 19:23:33 +0200

    Added group info to view

--- resources/views/groupinfo.blade.php
+++ resources/views/groupinfo.blade.php
@@ -3,7 +3,23 @@
 @section('content')
 <div class="container">
 
-    <h2>Test</h2>
+    <div class="row">
+        <div class="col-12">
+            <h3>{{$group->name}}</h3>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-12">
+            @foreach ($group->users as $user)
+                <hr>
+                {{$user->name}}<br>
+                {{$user->email}}<br>
+                {{$user->id}}
+            @endforeach
+        </div>
+    </div>
+
 
 </div>
 @endsection
