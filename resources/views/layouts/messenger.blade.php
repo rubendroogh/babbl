cef28bdaa28c3d7dc115be3dc12f2290eeddde2c
rubensPC <rubendroogh@hotmail.nl>
2018-09-19 18:14:22 +0200

    Added Bootstrap directly to scss files instead of linking from a server.

--- resources/views/layouts/messenger.blade.php
+++ resources/views/layouts/messenger.blade.php
@@ -17,7 +17,6 @@
     <link rel="shortcut icon" href="{{ asset('img/logo.ico') }}" />
 
     <!-- Styles -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/solid.css" integrity="sha384-VGP9aw4WtGH/uPAOseYxZ+Vz/vaTb1ehm1bwx92Fm8dTrE+3boLfF1SpAtB1z7HW" crossorigin="anonymous">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/fontawesome.css" integrity="sha384-1rquJLNOM3ijoueaaeS5m+McXPJCGdr5HcA03/VHXxcp2kX2sUrQDmFc3jR5i/C7" crossorigin="anonymous">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
