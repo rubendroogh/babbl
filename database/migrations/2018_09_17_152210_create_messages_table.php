83305199c53f0e8fe7203a956bee668c4feb1d31
RenzeD98 <you@example.com>
2018-09-20 19:24:06 +0200

    Added type to create_messages_table migration.

--- database/migrations/2018_09_17_152210_create_messages_table.php
+++ database/migrations/2018_09_17_152210_create_messages_table.php
@@ -17,6 +17,7 @@ class CreateMessagesTable extends Migration
             $table->increments('id');
             $table->string('content', 255);
             $table->unsignedTinyInteger('read')->default('0');
+            $table->string('type', 255);
             $table->unsignedInteger('user_id');
             $table->unsignedInteger('group_id');
             $table->timestamps();
