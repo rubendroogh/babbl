95740605ec569482083ac04f5f64f004ece4f677
rubensPC <rubendroogh@hotmail.nl>
2018-09-22 00:57:26 +0200

    Reworked styling in messenger view for better responsiveness.

--- public/js/messenger.js
+++ public/js/messenger.js
@@ -13953,7 +13953,7 @@ if (token) {
   var undefined;
 
   /** Used as the semantic version number. */
-  var VERSION = '4.17.11';
+  var VERSION = '4.17.10';
 
   /** Used as the size to enable large array optimizations. */
   var LARGE_ARRAY_SIZE = 200;
@@ -14217,7 +14217,7 @@ if (token) {
   var reHasUnicode = RegExp('[' + rsZWJ + rsAstralRange  + rsComboRange + rsVarRange + ']');
 
   /** Used to detect strings that need a more robust regexp to match words. */
-  var reHasUnicodeWord = /[a-z][A-Z]|[A-Z]{2}[a-z]|[0-9][a-zA-Z]|[a-zA-Z][0-9]|[^a-zA-Z0-9 ]/;
+  var reHasUnicodeWord = /[a-z][A-Z]|[A-Z]{2,}[a-z]|[0-9][a-zA-Z]|[a-zA-Z][0-9]|[^a-zA-Z0-9 ]/;
 
   /** Used to assign default `context` object properties. */
   var contextProps = [
@@ -15163,6 +15163,20 @@ if (token) {
       }
     }
     return result;
+  }
+
+  /**
+   * Gets the value at `key`, unless `key` is "__proto__".
+   *
+   * @private
+   * @param {Object} object The object to query.
+   * @param {string} key The key of the property to get.
+   * @returns {*} Returns the property value.
+   */
+  function safeGet(object, key) {
+    return key == '__proto__'
+      ? undefined
+      : object[key];
   }
 
   /**
@@ -17622,7 +17636,7 @@ if (token) {
           if (isArguments(objValue)) {
             newValue = toPlainObject(objValue);
           }
-          else if (!isObject(objValue) || isFunction(objValue)) {
+          else if (!isObject(objValue) || (srcIndex && isFunction(objValue))) {
             newValue = initCloneObject(srcValue);
           }
         }
@@ -20543,22 +20557,6 @@ if (token) {
         array[length] = isIndex(index, arrLength) ? oldArray[index] : undefined;
       }
       return array;
-    }
-
-    /**
-     * Gets the value at `key`, unless `key` is "__proto__".
-     *
-     * @private
-     * @param {Object} object The object to query.
-     * @param {string} key The key of the property to get.
-     * @returns {*} Returns the property value.
-     */
-    function safeGet(object, key) {
-      if (key == '__proto__') {
-        return;
-      }
-
-      return object[key];
     }
 
     /**
@@ -47181,6 +47179,7 @@ var group_id = $('#group_id').val();
 // Scroll down on init
 
 scrollToLastMessage();
+messagesRead();
 
 // Notifications
 
@@ -47335,7 +47334,7 @@ function getDateInFormat() {
 }
 
 // Message read
-messagesRead();
+
 
 function messagesRead() {
     var user_id = $('#user_id').val(),
--- resources/views/messenger.blade.php
+++ resources/views/messenger.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.messenger')
 
 @section('content')
-<div class="container">
+<div class="container-fluid">
     <div class="group_info collapse" id="collapseGroupInfo">
         <br>
 
@@ -26,39 +26,46 @@
         </div>
     </div>
 
-    <div id="messages" class="messages">
-        @foreach( $group->messages as $m )
-            <div class="fullwidth">
-                <div class="message_{{ $m->user == Auth::user() ? 'sent' : 'received' }}">
-                    @if( $m->user != Auth::user() )
-                        <small>{{ $m->user->name }}</small>
-                        <br>
-                    @endif
-                    {{ $m->content }}
-                    <br>
-                    <small>{{ $m->created_at }}</small>
-                    @if( $m->user == Auth::user() )
-                        <small id="messageRead{{ $m->id }}">{{ $m->read }}</small>
-                    @endif
-                    
-                </div>
+    <div class="row messages">
+        <div class="col-12">
+            <div id="messages">
+                @foreach( $group->messages as $m )
+                    <div class="fullwidth">
+                        <div class="message_{{ $m->user == Auth::user() ? 'sent' : 'received' }}">
+                            @if( $m->user != Auth::user() )
+                                <small>{{ $m->user->name }}</small>
+                                <br>
+                            @endif
+                            {{ $m->content }}
+                            <br>
+                            <small>{{ $m->created_at }}</small>
+                            @if( $m->user == Auth::user() )
+                                <small id="messageRead{{ $m->id }}">{{ $m->read }}</small>
+                            @endif
+                            
+                        </div>
+                    </div>
+                @endforeach
             </div>
-        @endforeach
+        </div>
     </div>
 
-    <form id="messageInput" class="message_input py-3 row bg-dark">
-        @csrf
-        <input id="user_id" type="hidden" name="_user_id" value="{{ Auth::user()->id }}">
-        <input id="group_id" type="hidden" name="_group_id" value="{{ $group->id }}">
-        <input id="user_name" type="hidden" name="_user_name" value="{{ Auth::user()->name }}">
-        <div class="col-9">
-            <input class="form-control" id="message" type="text" name="message" autofocus>
+    <div class="row bottom-absolute bg-dark">
+        <div class="col-12">
+            <form id="messageInput" class="message_input py-3 row">
+                @csrf
+                <input id="user_id" type="hidden" name="_user_id" value="{{ Auth::user()->id }}">
+                <input id="group_id" type="hidden" name="_group_id" value="{{ $group->id }}">
+                <input id="user_name" type="hidden" name="_user_name" value="{{ Auth::user()->name }}">
+                <div class="col-9">
+                    <input class="form-control" id="message" type="text" name="message" autofocus>
+                </div>
+                <div class="col-3">
+                    <input type="submit" class="btn btn-chatboy" id="sendMessageButton" value="Send">
+                </div>
+            </form>
         </div>
-        <div class="col-3">
-            <input type="submit" class="btn btn-chatboy" id="sendMessageButton" value="Send">
-        </div>
-    </form>
-
+    </div>
 </div>
 <script src="https://js.pusher.com/4.3/pusher.min.js"></script>
 <script src="/js/messenger.js"></script>
