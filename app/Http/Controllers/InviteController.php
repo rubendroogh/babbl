12616fba93fa6d00873d3aed7f773a7ba62e42fc
rubendroogh <rubendroogh@hotmail.nl>
2018-10-05 20:53:42 +0200

    Added invite controller, model and migration.

---
+++ app/Http/Controllers/InviteController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class InviteController extends Controller
+{
+	public function index()
+	{
+		
+	}
+
+	public function send()
+	{
+
+	}
+
+	public function accept()
+	{
+		
+	}
+}
