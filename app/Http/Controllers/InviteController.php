0f6f1d8995bd878dc6492d0d2e6fdfce17cfd7c2
rubensPC <rubendroogh@hotmail.nl>
2018-10-06 16:26:57 +0200

    Filled invite model.

--- app/Http/Controllers/InviteController.php
+++ app/Http/Controllers/InviteController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Invite;
 
 class InviteController extends Controller
 {
 	public function index()
 	{
-		
+		$invites = Invite::all();
+		return view('invites', ['invites', $invites]);
 	}
 
 	public function send()
--- app/Invite.php
+++ app/Invite.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Invite extends Model
 {
-    //
+    protected $fillable = [
+        'group_id', 'user_id'
+    ];
+
+    public function group()
+    {
+        return $this->belongsTo('App\Group');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
 }
